80af3e6a709fd7eb234af9b76d4788134168ec97
Raja Sekhar Manda <raja.m@syntaxnow.com>
2026-02-13 08:56:46 +0530

    Refactor Firebase configuration to use FileInputStream for credentials

--- src/main/java/com/syntaxnow/identity/config/FirebaseConfig.java
+++ src/main/java/com/syntaxnow/identity/config/FirebaseConfig.java
@@ -3,30 +3,33 @@ package com.syntaxnow.identity.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
+
     @Bean
     public FirebaseApp firebaseApp(
             @Value("${firebase.credentials}") String credentialsPath
     ) throws IOException {
-        InputStream serviceAccount =
-                new ClassPathResource(credentialsPath)
-                        .getInputStream();
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
 
-        return FirebaseApp.initializeApp(options);
+        log.info("Initializing Firebase using credentials from: {}", credentialsPath);
+
+        try (InputStream serviceAccount = new FileInputStream(credentialsPath)) {
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            return FirebaseApp.initializeApp(options);
+        }
     }
 }
